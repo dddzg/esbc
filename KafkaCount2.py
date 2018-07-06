5c0e99fc4e5888fc3361dd83bafd69d701dacd46
poohRui <32978179+poohRui@users.noreply.github.com>
2018-07-06 18:55:52 +0800

    Add files via upload

---
+++ KafkaCount2.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+ 
+file_path = "/test/v2"
+sc=SparkContext("spark://116.56.136.57:7077","KafkaCount")
+
+ssc=StreamingContext(sc,10)
+broker="116.56.136.57:9092"
+#打开一个TCP socket 地址 和 端口号
+topic="dzg"
+groupid="test-consumer-group"
+
+lines = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": broker})
+
+data=lines.map(lambda x:x[1]) #注意 取tuple下的第二个即为接收到的kafka流
+
+
+def map_split(x):
+    try:
+        date,ids,login = x.split(';')
+        return (date[:-2]+'00',1 if login =='0' else 0)
+    except BaseException:
+        return ('0',0)
+
+
+    
+lines1 = data.map(map_split).reduce(lambda x,y:(x[0],x[1]+y[1])).map(lambda x:x[0]+';'+str(x[1]))
+
+
+lines1.saveAsTextFiles(file_path)
+
+
+lines1.pprint()
+#启动spark streaming应用
+ssc.start()
+
+#等待计算终止
+ssc.awaitTermination()
+
