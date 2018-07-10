7a3fb9293c9c18a4e3db5a36361760b1a9e3f44b
poohRui <32978179+poohRui@users.noreply.github.com>
2018-07-10 20:33:25 +0800

    Update ShowUVSocketServer.java

--- ReceiveShowUV/src/main/java/ShowUVSocketServer.java
+++ ReceiveShowUV/src/main/java/ShowUVSocketServer.java
@@ -1,6 +1,7 @@
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
@@ -14,6 +15,8 @@ import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
 
 /**
@@ -33,50 +36,75 @@ public class ShowUVSocketServer extends WebSocketServer {
         super(address);
     }
 
-    private static final String FILE_PATH = "hdfs://master:9000/";
+    private static final String FILE_PATH = "hdfs://master:9000/test";
 
-    private static final int time = 1000 * 30;
+    private static final int time = 1000 * 10;
+
     public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
         System.out.println("open the WebsocketServer");
-//        Configuration conf = new Configuration();
-//
-//        Path path = new Path(FILE_PATH);
-//        FileSystem fs = null;
-//
-//        while(!fs.exists(path){
-//    }
-//        StringBuffer buffer = new StringBuffer();
-//        FSDataInputStream fsr = null;
-//        BufferedReader bufferReader = null;
-//        String lineText = null;
-//        URI uri;
-//        try {
-//            uri = new URI(FILE_PATH);
-//            fs = FileSystem.get(uri, conf);
-//
-//            fsr = fs.open(path);
-//
-//            bufferReader = new BufferedReader(new InputStreamReader(fsr));
-//
-//            while ((lineText = bufferReader.readLine()) != null) {
-//                sendUVData(webSocket, lineText);
-//                Thread.sleep(1000*30);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            IOUtils.closeStream(fsr);
-//        }
+        Configuration conf = new Configuration();
+
+        Path path = new Path(FILE_PATH);
+        FileSystem fs = null;
 
 
-        try {
-            sendUVData(webSocket,"");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while (true) {
+            StringBuffer buffer = new StringBuffer();
+            FSDataInputStream fsr = null;
+            BufferedReader bufferReader = null;
+            String lineText = null;
+            //文件目录
+            FileStatus[] status = null;
+            URI uri;
+            try {
+                uri = new URI(FILE_PATH);
+                fs = FileSystem.newInstance(uri, conf);
+
+                try {
+                    while (!fs.exists(path)) {
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                while (fs.listStatus(path) == null || fs.listStatus(path).length <= 0) {
+                }
+                status = fs.listStatus(path);
+
+                //根据文件夹修改时间进行降序
+                Arrays.sort(status, new Comparator<FileStatus>() {
+                    public int compare(FileStatus o1, FileStatus o2) {
+                        return o1.getModificationTime() > o2.getModificationTime() ? -1 : 1;
+                    }
+                });
+
+                //读取文件夹下最新的数据文件夹
+                Path logPath = status[0].getPath();
+                System.out.println(logPath.toString());
+                FileSystem logFs = FileSystem.newInstance(logPath.toUri(), conf);
+                FileStatus[] logStatus = logFs.listStatus(logPath);
+                if (logStatus != null) {
+                    for (FileStatus file : logStatus) {
+                        System.out.println(file.getPath().getName());
+                        if (!file.getPath().getName().startsWith("part-")) {
+                            continue;
+                        } else {
+                            FileSystem fileFs = FileSystem.newInstance(file.getPath().toUri(), conf);
+                            fsr = fileFs.open(file.getPath());
+                            bufferReader = new BufferedReader(new InputStreamReader(fsr));
+                            if (null != (lineText = bufferReader.readLine())) {
+                                System.out.println(lineText);
+                                webSocket.send(lineText);
+                            }
+                        }
+                    }
+                }
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
     }
-
 
     @Override
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
